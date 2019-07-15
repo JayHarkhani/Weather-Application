47cb02e1c01d940d34bbbb8827e48e573a77bf35
Jay Harkhani <jayharkhami999@gmail.com>
2019-07-15 11:57:48 +0530

    Add about text

--- public/js/style.js
+++ public/js/style.js
@@ -4,3 +4,4 @@ function onMouseMove(event) {
   gradient.style.backgroundImage = 'radial-gradient(at ' + event.clientX + 'px ' + event.clientY + 'px, rgba(159,0,191,.9) 0, #4D4FA7 70%)';
 }
 document.addEventListener("mousemove", onMouseMove);
+ 
